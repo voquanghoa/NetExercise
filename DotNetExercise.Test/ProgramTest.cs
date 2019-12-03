1e19c5917d38d6070ac51c327e7e8e43dcaf50d3
Vo Quang Hoa <voquanghoa@gmail.com>
2019-12-03 14:54:07 +0700

    Finish exercise 1

---
+++ DotNetExercise.Test/ProgramTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetExercise.Test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            Assert.AreEqual(Program.Sum(8, 6), 14);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Assert.AreEqual(Program.Sub(8, 6), 2);
+        }
+    }
+}
--- DotNetExercise/Program.cs
+++ DotNetExercise/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace DotNetExercise
 {
-    class Program
+    public class Program
     {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(Sum(7, 9));
+            Console.ReadKey();
         }
     }
 }
